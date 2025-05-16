2a6ca451c2e06aaab84cda3c23db92c7930507ce
Luka Ramishvili <ramishaforbusiness@gmail.com>
2025-05-17 01:25:20 +0200

    ad

--- components/schedule/schedule.jsx
+++ components/schedule/schedule.jsx
@@ -1,14 +1,9 @@
+import { getSchedule } from "@/lib/getSchedule";
 import classes from "./schedule.module.css";
 import Link from "next/link";
 
-export default function Schedule() {
-  const schedule = [
-    {
-      date: "15-05-2025",
-      city: "Tbilisi, Georgia",
-      url: "https://www.facebook.com/photo.php?fbid=122167395254359425&set=a.122096175458359425&type=3&mibextid=wwXIfr&rdid=ousA3hNT1afUgmXJ&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F194NBnc2rB%2F%3Fmibextid%3DwwXIfr#",
-    },
-  ];
+export default async function Schedule() {
+  const schedule = await getSchedule();
 
   return (
     <section className={classes.scheduleSection} id="Schedule">
